9b0c8037a2ab58e1bef4b75c0e71040b47dbf4dc
Terry Sikes <tsikes@tapestrysolutions.com>
2018-03-22 15:23:23 -0400

    Fixed Heroku port issue

--- server/server.js
+++ server/server.js
@@ -13,6 +13,6 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(root, 'index.html'));
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('server running...');
 });
